e873aa33b45473bc0177cf72a3219afdaf763f47
harrisonblackburn <blackbhr@yahoo.com>
2021-04-30 11:25:50 -0500

    dependencies and paths installed

--- server.js
+++ server.js
@@ -7,15 +7,16 @@ const fs = require('fs');
 // Sets up the Express App
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
+app.use(express.static(path.join(__dirname,'../assets/public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/assets/public/index.html')));
 
-app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/assets/public/notes.html')));
