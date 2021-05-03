3f1602f95b2891c533bc74bfd4b67456a94972c6
harrisonblackburn <blackbhr@yahoo.com>
2021-05-03 13:26:52 -0500

    functions installed

--- server.js
+++ server.js
@@ -1,22 +1,90 @@
 // Dependencies
 
 const express = require('express');
+
 const path = require('path');
+
 const fs = require('fs');
 
-// Sets up the Express App
-
 const app = express();
+
 const PORT = process.env.PORT || 8080;
 
+const allNotes = require('./db.json');
+
 // Sets up the Express app to handle data parsing
-app.use(express.static(path.join(__dirname,'../assets/public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('/public'));
+
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
+});
+
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
+        }
+    }
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
 
 
 
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/assets/public/index.html')));
-
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/assets/public/notes.html')));
+app.listen(PORT, () => {
+    console.log(`API server now listening on port ${PORT}`);
+});
